ab50ffdc36286911a7014cc0b2dd79c4adf2c0a0
Bang Nguyen <bangnguyen@motorola.com>
2018-10-08 19:43:14 +0800

    drm: msm: Create sysfs to expose panel vendor infor
    Create sysfs panelId, panelVer, and panelName in
    /sys/class/drm/card0-DSI-1 and /sys/class/drm/card0-DSI-2 to expose
    panel_name, panel_id and panel_ver for panel vendor information.
    
    These are needed for the TCMD/dropbox to query for the panel
    vendor information
    
    Change-Id: Ib452847f8e857e368159dc0cb7de347b120c105c
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1339882
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- include/drm/drm_modes.h
+++ include/drm/drm_modes.h
@@ -155,6 +155,7 @@ enum drm_mode_status {
 #define DRM_MODE_MATCH_FLAGS (1 << 2)
 #define DRM_MODE_MATCH_3D_FLAGS (1 << 3)
 #define DRM_MODE_MATCH_ASPECT_RATIO (1 << 4)
+#define DRM_MODE_DISPLAY_NAME_LEN 128
 
 /**
  * struct drm_display_mode - DRM kernel-internal display mode structure
@@ -290,6 +291,11 @@ struct drm_display_mode {
 	int vsync_end;
 	int vtotal;
 	int vscan;
+
+	u64 panel_id;
+	u64 panel_ver;
+	char panel_name[DRM_MODE_DISPLAY_NAME_LEN];
+
 	/**
 	 * @flags:
 	 *
