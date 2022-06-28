811c192a0ff846cb3663dfd438d09060202e3b11
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-06-29 03:00:12 +0530

    fs/fuse: Import `fuse_shortcircuit`
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1429,6 +1429,9 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	struct fuse_package *fpack;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1439,6 +1442,14 @@ struct task_struct {
 	 * Do not put anything below here!
 	 */
 };
+
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+struct fuse_package {
+	bool fuse_open_req;
+	struct file *filp;
+	char *iname;
+};
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 static inline struct pid *task_pid(struct task_struct *task)
 {
